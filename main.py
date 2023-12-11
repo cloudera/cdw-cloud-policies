62ee4a7ee034161fd37b8aa9688993c6d7ea68c4
Roohi <roohisyeda@cloudera.com>
2023-12-11 15:53:56 -0800

    Move to generated folder, test with condition removed for CFCloudwatch

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ for file in files:
     myfile = 'aws-iam-policies/docs/' + file
     with open(myfile, "r+") as resultsFile:
         jsonData = json5.load(resultsFile)
-    writeRestrictedfile = 'aws-iam-policies/' + file.replace('-doc', '')
+    writeRestrictedfile = 'aws-iam-policies/generated/' + file.replace('-doc', '')
     with open(writeRestrictedfile, 'w') as f:
         json.dump(jsonData, f, indent=4)
-    writeRestrictedManagedArnfile = 'aws-iam-policies/' + file.replace('-doc', '-managedARN')
+    writeRestrictedManagedArnfile = 'aws-iam-policies/generated/' + file.replace('-doc', '-managedARN')
     with open(writeRestrictedManagedArnfile, 'w') as managedArn:
         for sid in jsonData['Statement']:
             actionsList = sid['Action']
